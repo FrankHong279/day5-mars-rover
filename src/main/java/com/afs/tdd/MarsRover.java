c65921b62ae40bfb9fcea2a15578da14527aceab
HONGFR <HONGFR@OOCL.COM>
2025-09-05 16:31:35 +0800

    refactor: extract Command and Direction to Constant

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -6,37 +6,46 @@ package com.afs.tdd;
  * @date 9/5/2025 3:12 PM
  */
 public class MarsRover {
+
+    public static final String NORTH = "N";
+    public static final String EAST = "E";
+    public static final String SOUTH = "S";
+    public static final String WEST = "W";
+    public static final String MOVE = "M";
+    public static final String RIGHT = "R";
+    public static final String LEFT = "L";
+
     public CarLocation report(CarLocation carLocation, String command) {
         String direction = carLocation.getDirection();
-        if (command.equals("M")) {
-            if (direction.equals("N")) {
+        if (command.equals(MOVE)) {
+            if (direction.equals(NORTH)) {
                 carLocation.setY(carLocation.getY() + 1);
-            } else if (direction.equals("E")) {
+            } else if (direction.equals(EAST)) {
                 carLocation.setX(carLocation.getX() + 1);
-            } else if (direction.equals("S")) {
+            } else if (direction.equals(SOUTH)) {
                 carLocation.setY(carLocation.getY() - 1);
-            } else if (direction.equals("W")) {
+            } else if (direction.equals(WEST)) {
                 carLocation.setX(carLocation.getX() - 1);
             }
-        } else if (command.equals("R")) {
-            if (direction.equals("N")) {
-                carLocation.setDirection("E");
-            } else if (direction.equals("E")) {
-                carLocation.setDirection("S");
-            } else if (direction.equals("S")) {
-                carLocation.setDirection("W");
-            } else if (direction.equals("W")) {
-                carLocation.setDirection("N");
+        } else if (command.equals(RIGHT)) {
+            if (direction.equals(NORTH)) {
+                carLocation.setDirection(EAST);
+            } else if (direction.equals(EAST)) {
+                carLocation.setDirection(SOUTH);
+            } else if (direction.equals(SOUTH)) {
+                carLocation.setDirection(WEST);
+            } else if (direction.equals(WEST)) {
+                carLocation.setDirection(NORTH);
             }
-        } else if (command.equals("L")) {
-            if (direction.equals("N")) {
-                carLocation.setDirection("W");
-            } else if (direction.equals("W")) {
-                carLocation.setDirection("S");
-            } else if (direction.equals("S")) {
-                carLocation.setDirection("E");
-            } else if (direction.equals("E")) {
-                carLocation.setDirection("N");
+        } else if (command.equals(LEFT)) {
+            if (direction.equals(NORTH)) {
+                carLocation.setDirection(WEST);
+            } else if (direction.equals(WEST)) {
+                carLocation.setDirection(SOUTH);
+            } else if (direction.equals(SOUTH)) {
+                carLocation.setDirection(EAST);
+            } else if (direction.equals(EAST)) {
+                carLocation.setDirection(NORTH);
             }
         }
         return carLocation;
